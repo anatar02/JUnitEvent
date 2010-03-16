5aae065135a863137e5146e09bdb9c27a22e17f8
Berin Loritsch <bloritsch@d-haven.org>
2010-03-16 16:26:53 -0400

    Fixed duplicate reporting, which also provided inflated processor times.  Currently I am getting roughly 2:1 speed improvements which is expected on a dual core machine.

--- src/main/java/org/dhaven/jue/api/results/TestCaseSummary.java
+++ src/main/java/org/dhaven/jue/api/results/TestCaseSummary.java
@@ -21,7 +21,8 @@ package org.dhaven.jue.api.results;
 
 import java.util.ArrayList;
 import java.util.Collection;
-import java.util.List;
+import java.util.HashSet;
+import java.util.Set;
 
 import org.dhaven.jue.api.event.Status;
 import org.dhaven.jue.api.event.TestEvent;
@@ -31,7 +32,7 @@ import org.dhaven.jue.api.event.TestEvent;
  * children tests.  For example, a TestCase has many individual tests.
  */
 public class TestCaseSummary extends TestSummary {
-    private List<TestSummary> children = new ArrayList<TestSummary>(10);
+    private Set<TestSummary> children = new HashSet<TestSummary>();
     private Status summaryStatus = Status.Started;
 
     public TestCaseSummary(TestEvent event) {
