e042e7a36823b15eb787cebd8a782852f4ca3737
Berin Loritsch <bloritsch@d-haven.org>
2010-03-23 14:24:05 -0400

    Refactor class collector.  Make it easier to handle new URL types.

--- src/main/java/org/dhaven/jue/api/ClassCollector.java
+++ src/main/java/org/dhaven/jue/api/ClassCollector.java
@@ -29,6 +29,10 @@ import java.util.*;
 import java.util.jar.JarEntry;
 import java.util.jar.JarFile;
 
+interface CallBack<T> {
+    void deliver(T object);
+}
+
 /**
  * The class collector is used to look through your classpath for classes that
  * satisfy your criteria.  The class collector is used by the JUnitEvent
@@ -36,14 +40,14 @@ import java.util.jar.JarFile;
  * annotations.  It can also be used by your tests to find all implementations
  * of an interface, or classes that extend base classes.
  */
-public class ClassCollector {
+public class ClassCollector implements CallBack<String> {
     private Class<? extends Annotation>[] methodAnnotations = null;
     private String basePackage = "";
     private boolean excludeInnerClasses = false;
     private boolean recurse = false;
     private File basePath;
     private ClassLoader loader = null;
-    private List<File> rootFiles;
+    private List<Class> collectedClasses = new ArrayList<Class>(20);
 
     public void setClassLoader(ClassLoader loader) {
         this.loader = loader;
@@ -66,134 +70,39 @@ public class ClassCollector {
     }
 
     public Class<?>[] collect() throws Exception {
-        ArrayList<Class<?>> classes = new ArrayList<Class<?>>(20);
+        for (Scanner scanner : createScanners()) {
+            scanner.scan();
+        }
 
-        if (null == basePath) {
-            if (null == rootFiles) {
-                try {
-                    Enumeration<URL> basePaths = getClassLoader().getResources("");
-                    List<URL> paths = Collections.list(basePaths);
-                    rootFiles = new ArrayList<File>(paths.size());
+        return this.collectedClasses.toArray(new Class<?>[collectedClasses.size()]);
+    }
 
-                    for (URL base : paths) {
-                        if ("file".equals(base.getProtocol())) {
-                            File path = new File(URLDecoder.decode(base.getFile(), "utf-8"));
+    private Iterable<Scanner> createScanners() throws IOException {
+        Collection<Scanner> scanners = new ArrayList<Scanner>(5);
+        if (null != basePath) {
+            scanners.add(new FileScanner(basePath, this));
+            return scanners;
+        }
 
-                            // shouldn't be anything else, but making sure.
-                            if (path.exists() && path.isDirectory()) {
-                                rootFiles.add(path);
-                            }
-                        }
-                    }
-                } catch (IOException e) {
-                    // Very unlikely that this would occur here.  Repackage in
-                    // an AssertionError.
-                    throw new AssertionError(e);
+        Enumeration<URL> basePaths = getClassLoader().getResources("");
+        List<URL> paths = Collections.list(basePaths);
+
+        for (URL base : paths) {
+            if ("file".equals(base.getProtocol())) {
+                File path = new File(URLDecoder.decode(base.getFile(), "utf-8"));
+
+                // shouldn't be anything else, but better safe than
+                // sorry.
+                if (path.exists() && path.isDirectory()) {
+                    scanners.add(new FileScanner(path, this));
                 }
-
-            }
-
-            List<URL> paths = Collections.list(getClassLoader().getResources(basePackage.replace('.', '/')));
-
-            for (URL path : paths) {
-                addClassesFromPath(classes, path);
-            }
-        } else {
-            if (null == rootFiles) {
-                rootFiles = Arrays.asList(basePath);
-            }
-
-            handleFile(classes, basePath);
-        }
-
-        return classes.toArray(new Class<?>[classes.size()]);
-    }
-
-    private void addClassesFromPath(ArrayList<Class<?>> classes, URL url) throws Exception {
-        if ("file".equals(url.getProtocol())) {
-            handleFile(classes, new File(URLDecoder.decode(url.getFile(), "utf-8")));
-        } else if ("jar".equals(url.getProtocol())) {
-            String jarName = url.getFile().substring(0, url.getFile().indexOf("!"));
-            handleJar(classes, new JarFile(jarName));
-        }
-    }
-
-    private void handleFile(ArrayList<Class<?>> classes, File base) {
-        File[] files = base.listFiles();
-
-        checkListing(base, files);
-
-        for (File file : files) {
-            if (file.getName().endsWith(".class")) {
-                String className = file.getPath();
-                for (File root : rootFiles) {
-                    if (className.startsWith(root.getPath())) {
-                        className = className.substring(root.getPath().length() + 1);
-                    }
-                }
-                className = className.substring(0, className.length() - ".class".length());
-                className = className.replace(File.separatorChar, '.');
-
-                addClassIfMatches(classes, className);
-            } else if (recurse && file.isDirectory()) {
-                handleFile(classes, file);
+            } else if ("jar".equals(base.getProtocol())) {
+                String jarName = base.getFile().substring(0, base.getFile().indexOf("!"));
+                scanners.add(new JarScanner(new JarFile(jarName), this));
             }
         }
-    }
 
-    private void checkListing(File base, File[] files) {
-        if (null == files) {
-            if (base.exists()) {
-                throw new IllegalStateException(
-                        base.isDirectory() ?
-                                "Dang Java implementers thought it was a good idea " +
-                                        "to return null instead of throw an exception when" +
-                                        " it couldn't list files for a valid directory."
-                                : base.getPath() + " is not a directory");
-            } else {
-                throw new IllegalStateException(base.getPath() + " does not exist");
-            }
-        }
-    }
-
-    private void handleJar(ArrayList<Class<?>> classes, JarFile file) {
-        for (JarEntry entry : Collections.list(file.entries())) {
-            String className = entry.getName();
-            className = className.substring(0, className.length() - ".class".length());
-            className = className.replace('/', '.');
-
-            if (className.startsWith(basePackage)) {
-                if (recurse) {
-                    addClassIfMatches(classes, className);
-                } else if (className.substring(basePackage.length()).lastIndexOf('.') <= 0) {
-                    addClassIfMatches(classes, className);
-                }
-            }
-        }
-    }
-
-    private void addClassIfMatches(ArrayList<Class<?>> classes, String className) {
-        Class<?> classToCheck;
-
-        try {
-            classToCheck = Class.forName(className, true, getClassLoader());
-        } catch (ClassNotFoundException exception) {
-            System.out.println("Could not find class: " + className);
-            return;
-        }
-        boolean matches = true;
-
-        if (null != methodAnnotations) {
-            matches = checkMethodAnnotations(classToCheck);
-        }
-
-        if (matches && excludeInnerClasses) {
-            matches = classToCheck.getEnclosingClass() == null;
-        }
-
-        if (matches) {
-            classes.add(classToCheck);
-        }
+        return scanners;
     }
 
     private boolean checkMethodAnnotations(Class<?> classToCheck) {
@@ -221,5 +130,118 @@ public class ClassCollector {
     public void setBasePath(File path) {
         assert path.exists() && path.isDirectory();
         basePath = path;
+    }
+
+    @Override
+    public void deliver(final String className) {
+        if (null != basePackage && !className.startsWith(basePackage)) {
+            return;
+        }
+
+        if (!recurse && className.substring(basePackage == null ?
+                0 : basePackage.length()).lastIndexOf('.') > 0) {
+            return;
+        }
+
+        Class<?> classToCheck;
+
+        try {
+            classToCheck = Class.forName(className, true, getClassLoader());
+        } catch (ClassNotFoundException exception) {
+            System.out.println("Could not find class: " + className);
+            return;
+        }
+
+        boolean matches = true;
+
+        if (null != methodAnnotations) {
+            matches = checkMethodAnnotations(classToCheck);
+        }
+
+        if (matches && excludeInnerClasses) {
+            matches = classToCheck.getEnclosingClass() == null;
+        }
+
+        if (matches) {
+            collectedClasses.add(classToCheck);
+        }
+    }
+
+    private interface Scanner {
+        void scan();
+    }
+
+    private final static class FileScanner implements Scanner {
+        private final File root;
+        private final CallBack<String> handler;
+
+        public FileScanner(File root, CallBack<String> handler) {
+            this.root = root;
+            this.handler = handler;
+        }
+
+        public void scan() {
+            scanDirectory(root);
+        }
+
+        public void scanDirectory(File dir) {
+            File[] files = dir.listFiles();
+
+            checkListing(dir, files);
+
+            for (File file : files) {
+                if (file.getName().endsWith(".class")) {
+                    String className = file.getPath();
+
+                    // should always be true...
+                    if (className.startsWith(root.getPath())) {
+                        className = className.substring(root.getPath().length() + 1);
+                    }
+
+                    className = className.substring(0, className.length() - ".class".length());
+                    className = className.replace(File.separatorChar, '.');
+
+                    handler.deliver(className);
+                } else if (file.isDirectory()) {
+                    scanDirectory(file);
+                }
+            }
+        }
+
+        private void checkListing(File base, File[] files) {
+            if (null == files) {
+                if (base.exists()) {
+                    throw new IllegalStateException(
+                            base.isDirectory() ?
+                                    "Dang Java implementers thought it was a good idea " +
+                                            "to return null instead of throw an exception when" +
+                                            " it couldn't list files for a valid directory."
+                                    : base.getPath() + " is not a directory");
+                } else {
+                    throw new IllegalStateException(base.getPath() + " does not exist");
+                }
+            }
+        }
+    }
+
+    private final static class JarScanner implements Scanner {
+        private final JarFile jar;
+        private final CallBack<String> handler;
+
+        public JarScanner(JarFile jar, CallBack<String> handler) {
+            this.jar = jar;
+            this.handler = handler;
+        }
+
+        @Override
+        public void scan() {
+            for (JarEntry entry : Collections.list(jar.entries())) {
+                String className = entry.getName();
+                className = className.substring(0, className.length() - ".class".length());
+                className = className.replace('/', '.');
+
+                handler.deliver(className);
+            }
+        }
     }
 }
